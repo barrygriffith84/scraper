ece351ce24f003c364fe75b06c70d639e8c966ad
Barry Griffith <barrygriffith@outlook.com>
2020-09-20 01:43:07 -0400

    Can scrape all of the county names and populations

---
+++ scraper/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace scraper
+{
+   public class Location
+    {
+        public string Name { get; set; }
+
+        public int Population { get; set; }
+    }
+}
